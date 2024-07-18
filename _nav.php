146f3ccaf7cff921f6300082278c29ca95ade831
HARSH KAMAL <160395089+Harsh12kamal@users.noreply.github.com>
2024-07-18 10:55:48 +0530

    Add files via upload

---
+++ _nav.php
@@ -0,0 +1,25 @@
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top " style="height: 100%;border-radius: 5px;">
+  <div class="container-fluid" style="max-height: 30px; width: 100%;">
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse bg-light" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/php/login%20system/welcome.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/php/login%20system/login.php">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/php/login%20system/logout.php">logout</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="/php/login%20system/signup.php" >Sign up</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
